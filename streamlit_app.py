9d2fac06562937c3b53b41d2192df9c279ee8d76
rasmusengelbrecht <32791593+rasmusengelbrecht@users.noreply.github.com>
2023-06-12 19:30:07 +0200

    Added new stuff e.g. upload functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,38 +17,59 @@ input_col1, input_col2 = st.columns(2)
 team_member_input = input_col1.text_input("Enter the names of team members (comma-separated)", "John, Jane, Alice, Bob")
 team_member_list = [member.strip() for member in team_member_input.split(",")]
 
-# Predefined capabilities based on team type
-predefined_capabilities = {
-    "Data": "Statistics, Visualization, SQL, Programming, Spreadsheets, Storytelling",
-    "UX": "User Research, Wireframing, Prototyping, Usability Testing, Interaction Design",
-    "Design": "Graphic Design, Typography, Color Theory, Layout, Branding",
-    "Product": "Product Strategy, Market Research, Agile Methodology, User Feedback, Roadmapping",
-    "Engineering": "Software Development, Algorithms, Data Structures, Testing, Debugging"
-}
-
-# Default capability list when no team type is selected
-default_capabilities = "Capability 1, Capability 2, Capability 3"
-
 # Capabilities
-capability_input = input_col2.text_input("Enter the capabilities (comma-separated)", default_capabilities)
+capability_input = input_col2.text_input("Enter the capabilities (comma-separated)", "Capability 1, Capability 2, Capability 3")
 capability_list = [capability.strip() for capability in capability_input.split(",")]
 
 # Team type selection
 show_team_type = input_col2.checkbox("Try a team template instead")
 if show_team_type:
     team_type = st.selectbox("Select Team Type", ("", "Data", "UX", "Design", "Product", "Engineering"))
-    if team_type in predefined_capabilities:
-        capability_input = predefined_capabilities[team_type]
+    if team_type:
+        template_capabilities = {
+            "Data": "Statistics, Visualization, SQL, Programming, Spreadsheets, Storytelling",
+            "UX": "User Research, Wireframing, Prototyping, Usability Testing, Interaction Design",
+            "Design": "Graphic Design, Typography, Color Theory, Layout, Branding",
+            "Product": "Product Strategy, Market Research, Agile Methodology, User Feedback, Roadmapping",
+            "Engineering": "Software Development, Algorithms, Data Structures, Testing, Debugging"
+        }
+        capability_input = template_capabilities.get(team_type, "")
         capability_list = [capability.strip() for capability in capability_input.split(",")]
+
+
+##################### CSV Upload and Export #####################
+with st.expander("Saved data from earlier? Upload your CSV here!"):
+
+    # CSV upload
+    uploaded_file = st.file_uploader("Upload CSV file", type=["csv", "txt"])
+
+    if uploaded_file is not None:
+        # Read the uploaded file
+        if uploaded_file.name.lower().endswith('.csv'):
+            delimiter = ','
+        else:
+            delimiter = ';'
+        csv_data = pd.read_csv(uploaded_file, delimiter=delimiter)
+
+        # Extract team members and capability ratings
+        team_member_list = csv_data.columns[1:].tolist()
+        capability_list = csv_data.iloc[:, 0].tolist()
+        capability_ratings = csv_data.iloc[:, 1:].values.tolist()
+
+        # Create DataFrame for stacked radar chart of the entire team
+        team_df = pd.DataFrame(capability_ratings, columns=team_member_list, index=capability_list)
+
+    else:
+        # Create DataFrame for stacked radar chart of the entire team
+        team_df = pd.DataFrame(index=capability_list)
+        for member in team_member_list:
+            team_df[member] = [5] * len(capability_list)
 
 # Add space
 st.markdown("---")
 
 ##################### Team Member Section #####################
 st.header("Team Members \U0001F9B8")
-
-# Create DataFrame for stacked radar chart of the entire team
-team_df = pd.DataFrame(index=capability_list)
 
 # Display team members in two columns
 num_columns = 2
@@ -67,7 +88,7 @@ color_palette = [
 # Iterate over team members and display their capabilities
 for i, member in enumerate(team_member_list):
     # Capability ratings
-    ratings_data = []
+    ratings_data = team_df[member].values.tolist()
     with col1 if i % num_columns == 0 else col2:
         st.header(member)
 
@@ -76,8 +97,11 @@ for i, member in enumerate(team_member_list):
 
         for j, capability in enumerate(capability_list):
             key = f"{member}_{j}_rating"  # Unique key for each slider
-            rating = rating_col.slider(f"{capability}", 1, 10, 5, key=key)
-            ratings_data.append(rating)
+            rating = rating_col.slider(f"{capability}", 1, 10, ratings_data[j], key=key)
+            ratings_data[j] = rating
+
+        # Update DataFrame with capability ratings
+        team_df[member] = ratings_data
 
         # Create DataFrame for radar chart
         df = pd.DataFrame({member: ratings_data}, index=capability_list)
@@ -103,9 +127,6 @@ for i, member in enumerate(team_member_list):
 
         # Display radar chart for the team member
         chart_col.plotly_chart(fig, use_container_width=True)
-
-        # Add ratings to team DataFrame
-        team_df[member] = ratings_data
 
 # Add space
 st.markdown("---")
@@ -159,14 +180,11 @@ if highest_capability != lowest_capability:
 # Display stacked radar chart for the entire team
 st.plotly_chart(team_fig, use_container_width=True)
 
-# Add space
-st.markdown("---")
-
 ##################### CSV Export #####################
-st.header("Export as CSV")
-export_csv = team_df.copy()  # Create a copy of the team DataFrame
-export_csv.reset_index(inplace=True)  # Reset the index
-csv = export_csv.to_csv(index=False)  # Convert DataFrame to CSV format
-b64 = base64.b64encode(csv.encode()).decode()  # Encode CSV data as base64
-href = f'<a href="data:file/csv;base64,{b64}" download="team_capabilities.csv">Download CSV File</a>'
-st.markdown(href, unsafe_allow_html=True)
+csv_export = team_df.reset_index().to_csv(index=False)  # Convert DataFrame to CSV format
+b64 = base64.b64encode(csv_export.encode()).decode()  # Encode CSV as base64
+href = f'<a href="data:file/csv;base64,{b64}" download="team_capabilities.csv">Download CSV</a>'  # Create download link
+st.markdown("---")
+st.markdown("### CSV Export")
+st.markdown("Click the link below to download the team capabilities CSV file:")
+st.markdown(href, unsafe_allow_html=True)  # Display download link
